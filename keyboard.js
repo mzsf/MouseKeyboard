17cbae40ff1a16a8f356db8fc5e7f50ebe754c86
Micah Sze <micahsze@gmail.com>
2018-03-25 14:05:46 -0700

    Updates
    Updated file - primarily with javascript to work with the shift key.

--- keyboard.js
+++ keyboard.js
@@ -1,49 +1,108 @@
-function addtextA(){document.myform.outputtext.value += "a";}
-function addtextB(){document.myform.outputtext.value += "b";}
-function addtextC(){document.myform.outputtext.value += "c";}
-function addtextD(){document.myform.outputtext.value += "d";}
-function addtextE(){document.myform.outputtext.value += "e";}
-function addtextF(){document.myform.outputtext.value += "f";}
-function addtextG(){document.myform.outputtext.value += "g";}
-function addtextH(){document.myform.outputtext.value += "h";}
-function addtextI(){document.myform.outputtext.value += "i";}
-function addtextJ(){document.myform.outputtext.value += "j";}
-function addtextK(){document.myform.outputtext.value += "k";}
-function addtextL(){document.myform.outputtext.value += "l";}
-function addtextM(){document.myform.outputtext.value += "m";}
-function addtextN(){document.myform.outputtext.value += "n";}
-function addtextO(){document.myform.outputtext.value += "o";}
-function addtextP(){document.myform.outputtext.value += "p";}
-function addtextQ(){document.myform.outputtext.value += "q";}
-function addtextR(){document.myform.outputtext.value += "r";}
-function addtextS(){document.myform.outputtext.value += "s";}
-function addtextT(){document.myform.outputtext.value += "t";}
-function addtextU(){document.myform.outputtext.value += "u";}
-function addtextV(){document.myform.outputtext.value += "v";}
-function addtextW(){document.myform.outputtext.value += "w";}
-function addtextX(){document.myform.outputtext.value += "x";}
-function addtextY(){document.myform.outputtext.value += "y";}
-function addtextZ(){document.myform.outputtext.value += "z";}
-function addtext1(){document.myform.outputtext.value += "1";}
-function addtext2(){document.myform.outputtext.value += "2";}
-function addtext3(){document.myform.outputtext.value += "3";}
-function addtext4(){document.myform.outputtext.value += "4";}
-function addtext5(){document.myform.outputtext.value += "5";}
-function addtext6(){document.myform.outputtext.value += "6";}
-function addtext7(){document.myform.outputtext.value += "7";}
-function addtext8(){document.myform.outputtext.value += "8";}
-function addtext9(){document.myform.outputtext.value += "9";}
-function addtext0(){document.myform.outputtext.value += "0";}
-function addtextdot(){document.myform.outputtext.value += ".";}
-function addtextwww(){document.myform.outputtext.value += "www.";}
-function addtexthttp(){document.myform.outputtext.value += "http://";}
-function addtextat(){document.myform.outputtext.value += "@";}
-function addtextcom(){document.myform.outputtext.value += ".com";}
-function addtextspace(){document.myform.outputtext.value += " ";}
-function addtextselect(){document.myform.outputtext.select();}
+function addtext(var_text){
+    // let container = document.getElementsByClassName('numbers')
+    // container[0].style.boxShadow = '1px 3px 2px rgba(0, 0, 0, 1.25)';
+    document.myform.outputtext.value += var_text;
+    // console.log(var_text);
+}
+
+function deltext(){
+    document.myform.outputtext.value = document.myform.outputtext.value.slice(0,document.myform.outputtext.value.length-1);
+}
+
+function chgUpperText(){
+    let alpha = document.getElementsByClassName('letters');
+    alpha = Array.from(alpha);
+    // console.log(alpha);
+    alpha.forEach(function(input){
+        input.value = input.value.toUpperCase();
+        input.outerHTML = `<input class=\"letters\" value=\"${input.value}\" onclick=\"addtext('${input.value}');\" type=\"button\">`;
+        // alpha.value = input.value.toUpperCase();
+        // console.log(alpha);
+    });
+    // console.log(alpha);
+}
+
+function chgLowerText(){
+    let alpha = document.getElementsByClassName('letters');
+    alpha = Array.from(alpha);
+    // console.log(alpha);
+    alpha.forEach(function(input){
+        input.value = input.value.toLowerCase();
+        input.outerHTML = `<input class=\"letters\" value=\"${input.value}\" onclick=\"addtext('${input.value}');\" type=\"button\">`;
+        // alpha.value = input.value.toLowerCase();
+        // console.log(alpha);
+    });
+    // console.log(alpha);
+}
+
+function chgUpperNum(){
+    let num = document.getElementsByClassName('numbers');
+    num = Array.from(num);
+    num[0].value = "!";
+    num[1].value = "@";
+    num[2].value = "#";
+    num[3].value = "$";
+    num[4].value = "%";
+    num[5].value = "^";
+    num[6].value = "&";
+    num[7].value = "*";
+    num[8].value = "(";
+    num[9].value = ")";
+    num[10].value = ",";
+    num[11].value = ";";
+
+    num.forEach(function(input, index){
+        // console.log(index);
+        input.outerHTML = `<input class=\"numbers\" value=\"${num[index].value}\" onclick=\"addtext('${num[index].value}');\" type=\"button\">`;
+    });
+}
+
+function chgLowerNum(){
+    let num = document.getElementsByClassName('numbers');
+    num = Array.from(num);
+    num[0].value = "1";
+    num[1].value = "2";
+    num[2].value = "3";
+    num[3].value = "4";
+    num[4].value = "5";
+    num[5].value = "6";
+    num[6].value = "7";
+    num[7].value = "8";
+    num[8].value = "9";
+    num[9].value = "0";
+    num[10].value = ".";
+    num[11].value = ":";
+
+    num.forEach(function(input, index){
+        // console.log(index);
+        input.outerHTML = `<input class=\"numbers\" value=\"${num[index].value}\" onclick=\"addtext('${num[index].value}');\" type=\"button\">`;
+    });
+}
+
+function toggleText(){
+    let status = document.querySelector('#shift').className;
+    if (status === 'off'){
+        chgUpperText();
+        chgUpperNum();
+        document.querySelector('#shift').className = 'on';
+        console.log('CAPSLOCK IS ON')
+    } else if (status === 'on') {
+        chgLowerText();
+        chgLowerNum();
+        document.querySelector('#shift').className = 'off';
+        console.log('CAPSLOCK IS OFF')
+    } else {
+        console.log('Error')
+    }
+}
+
+function addtextselect(){
+    document.myform.outputtext.select();
+}
+
 function myFunction() {
   /* Get the text field */
-  var copyText = document.getElementById("outputtext");
+  let copyText = document.getElementById("outputtext");
   /* Select the text field */
 
   copyText.select();
@@ -52,3 +111,6 @@ function myFunction() {
   /* Alert the copied text */
   alert("Copied the text: " + copyText.value);
 }
+
+// let container = document.getElementsByClassName('numbers')
+// container[0].style.boxShadow = '1px 3px 2px rgba(0, 0, 0, 1.25)';
